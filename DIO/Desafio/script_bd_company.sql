e15b16e767990c5072bf13685dfcd4a5e6b8c4c9
Tiago Ferrer de Souza Machado <43378200+TiagoFerrer@users.noreply.github.com>
2023-10-22 13:17:58 -0300

    Adequação do arquivo de criação das tabelas

--- DIO/Desafio/script_bd_company.sql
+++ DIO/Desafio/script_bd_company.sql
@@ -1,14 +1,9 @@
 create schema if not exists azure_company;
 use azure_company;
 
-select * from information_schema.table_constraints
-	where constraint_schema = 'azure_company';
-
--- restrição atribuida a um domínio
--- create domain D_num as int check(D_num> 0 and D_num< 21);
-
-CREATE TABLE employee(
-	Fname varchar(15) not null,
+-- Criação da tabela Funcionario
+CREATE TABLE Funcionario(
+    Fname varchar(15) not null,
     Minit char,
     Lname varchar(15) not null,
     Ssn char(9) not null, 
@@ -22,17 +17,18 @@ CREATE TABLE employee(
     constraint pk_employee primary key (Ssn)
 );
 
-alter table employee 
+alter table Funcionario 
 	add constraint fk_employee 
 	foreign key(Super_ssn) references employee(Ssn)
     on delete set null
     on update cascade;
 
-alter table employee modify Dno int not null default 1;
+alter table Funcionario modify Dno int not null default 1;
 
-desc employee;
+desc Funcionario;
 
-create table departament(
+-- Criação da tabela Departamento
+create table Departamento(
 	Dname varchar(15) not null,
     Dnumber int not null,
     Mgr_ssn char(9) not null,
@@ -44,30 +40,29 @@ create table departament(
     foreign key (Mgr_ssn) references employee(Ssn)
 );
 
--- 'def', 'company_constraints', 'departament_ibfk_1', 'company_constraints', 'departament', 'FOREIGN KEY', 'YES'
--- modificar uma constraint: drop e add
-alter table departament drop  departament_ibfk_1;
-alter table departament 
+alter table Departamento 
 		add constraint fk_dept foreign key(Mgr_ssn) references employee(Ssn)
         on update cascade;
 
-desc departament;
+desc Departamento;
 
-create table dept_locations(
+-- Criação da tabela Depart_Local
+create table Depart_Local(
 	Dnumber int not null,
 	Dlocation varchar(15) not null,
     constraint pk_dept_locations primary key (Dnumber, Dlocation),
     constraint fk_dept_locations foreign key (Dnumber) references departament (Dnumber)
 );
 
-alter table dept_locations drop fk_dept_locations;
+alter table Depart_Local drop fk_dept_locations;
 
-alter table dept_locations 
+alter table Depart_Local 
 	add constraint fk_dept_locations foreign key (Dnumber) references departament(Dnumber)
 	on delete cascade
     on update cascade;
 
-create table project(
+-- Criação da tabela Projeto
+create table Projeto(
 	Pname varchar(15) not null,
 	Pnumber int not null,
     Plocation varchar(15),
@@ -77,8 +72,8 @@ create table project(
     constraint fk_project foreign key (Dnum) references departament(Dnumber)
 );
 
-
-create table works_on(
+-- Criação da tabela Works_On
+create table Works_On(
 	Essn char(9) not null,
     Pno int not null,
     Hours decimal(3,1) not null,
@@ -87,8 +82,8 @@ create table works_on(
     constraint fk_project_works_on foreign key (Pno) references project(Pnumber)
 );
 
-drop table dependent;
-create table dependent(
+-- Criação da tabela Dependente
+create table Dependente(
 	Essn char(9) not null,
     Dependent_name varchar(15) not null,
     Sex char,
@@ -99,4 +94,3 @@ create table dependent(
 );
 
 show tables;
-desc dependent;
